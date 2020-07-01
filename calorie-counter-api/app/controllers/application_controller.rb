9c26880026333bb836608268033b426a91a388ef
callas4ever <thetseringnorbu@gmail.com>
2020-06-30 23:59:19 -0400

    that's it for now

--- calorie-counter-api/app/controllers/application_controller.rb
+++ calorie-counter-api/app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::API
+    def index
+    calorie_entries=CaloryEntry.all
+    render json: api_v1_calorie_entries_path
+    end
+    def create
+    calorie=CalorieEntry.creat(calorie_entries_params)
+    render json: api_v1_calorie_entry_path(calorie)
+    end
+
+
+
+
+
+    private 
+    def calorie_entries_params
+        params.require(:api_v1_calorie_entries).permit(:calorie, :note)
+    end
 end
